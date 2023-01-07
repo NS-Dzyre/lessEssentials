434df6937499c04e24de8b09e457e3a61ecb5c93
Cameron Peterson <cameron.peterson@wallawalla.edu>
2023-01-07 12:25:27 -0800

    push tag

--- src/me/Dzyre/lessEssentials/Main.java
+++ src/me/Dzyre/lessEssentials/Main.java
@@ -16,7 +16,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
